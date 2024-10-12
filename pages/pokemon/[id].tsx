db90073666fe85d8a0468e82ac96612065df3781
Thibault <th.carayon@gmail.com>
2024-10-12 17:33:54 +0200

    implement dynamic route handling in getServerSideProps

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Head from "next/head";
 
-import { Pokemon } from "../../interfaces/pokemon";
+import { Pokemon, PokemonPageProps } from "../../interfaces/pokemon";
 import { Layout } from "../../components/Layout";
+import { calculatePower } from "../../utils/calculatePower";
 
-const PokemonPage = (props: Pokemon) => {
+const PokemonPage = ({ pokemon }: PokemonPageProps) => {
+  const { name, id, hp, attack, defense, special_attack, special_defense, speed, power } = pokemon;
+
   return (
     <>
       <Head>
-        <title>{props.name}</title>
+        <title>{name}</title>
       </Head>
-      <h1>{props.name}</h1>
+      <h1>{name}</h1>
       <button>{`< Previous`}</button>
       <button>{`Next >`}</button>
     </>
@@ -19,28 +22,35 @@ const PokemonPage = (props: Pokemon) => {
 
 PokemonPage.getLayout = Layout;
 
-export async function getServerSideProps({
-  params: { id },
-}: {
-  params: { id: number };
-}) {
+export const getServerSideProps = async ({ params }: { params: { id: number } }) => {
+  const { id } = params; // Get the ID from the parameter
+
   try {
-    // Implement new endpoint in /api/pokemon/[id].ts and use it here
-    const pokemonExample: Pokemon = {
-      id: id,
-      name: "Bulbasaur",
-      type: ["Grass", "Poison"],
-      hp: 45,
-      attack: 49,
-      defense: 49,
-      special_attack: 65,
-      special_defense: 65,
-      speed: 45,
+    // Call the API to get the list of Pokémon
+    const response = await fetch(`http://localhost:3000/api/pokemon/${id}`); // Ensure the URL is correct
+    const pokemon: Pokemon = await response.json();
+
+    // Check if the pokemon has been found
+    if (!pokemon) {
+      return {
+        notFound: true, // If not found, return 404
+      };
+    }
+
+    const pokemonWithPower = {
+      ...pokemon,
+      power: calculatePower(pokemon)
     };
-    return { props: pokemonExample };
-  } catch (error) {
+
     return {
-      notFound: true,
+      props: {
+        pokemon: pokemonWithPower,
+      }
+    }
+  } catch (error) {
+    console.error("Error while fetching the Pokémon:", error);
+    return {
+      notFound: true, // Return 404 in case of error
     };
   }
 }

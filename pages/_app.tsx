b5ef0db92f4d64c6809ae729beefe534d591e1ab
Thibault <th.carayon@gmail.com>
2024-10-13 20:37:41 +0200

    feat: add loading screen for pokemon page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
-import { ReactElement, ReactNode } from "react";
+import { ReactElement, ReactNode, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import PokemonSkeleton from "../components/PokemonSkeleton";
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -17,8 +19,38 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
     ((page) => {
       return page;
     });
+  const router = useRouter();
 
-  return getLayout(<Component {...pageProps} />);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const start = (url: string) => {
+
+      // Check if the route matches pokemon/[id]
+      const isPokemonRoute = /^\/pokemon\/[^/]+$/.test(url);
+      if (isPokemonRoute) {
+        setLoading(true);
+      }
+    };
+    const end = () => {
+      console.log("Chargement terminé");
+      setLoading(false);
+    };
+    router.events.on("routeChangeStart", start);
+    router.events.on("routeChangeComplete", end);
+    router.events.on("routeChangeError", end);
+    return () => {
+      router.events.off("routeChangeStart", start);
+      router.events.off("routeChangeComplete", end);
+      router.events.off("routeChangeError", end);
+    };
+  }, []);
+  
+  if (loading) {
+    return getLayout(<PokemonSkeleton />);
+  }
+
+  return getLayout(<Component {...pageProps} />)
+
 }
 
 export default App;
